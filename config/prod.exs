95af61e315e7f6022e1cd4910b1076a93896d5b6
fpapado <fgpapado@gmail.com>
2017-01-03 00:47:10 +0200

    Add production ENV secret config

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,14 @@ use Mix.Config
 config :reverie, Reverie.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+# Configure Ecto Repository for Production
+config :reverie, Reverie.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
