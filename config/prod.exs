96d3c2d1657b6fcd170390b97cf32bcd880dda9b
fpapado <fgpapado@gmail.com>
2017-01-03 00:53:38 +0200

    Set up heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :reverie, Reverie.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "fierce-woodland-48343.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure Ecto Repository for Production
 config :reverie, Reverie.Repo,
