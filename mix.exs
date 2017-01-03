8f0d21a60a57b9b05f4153f724bf44c056f7d042
fpapado <fgpapado@gmail.com>
2017-01-03 02:47:58 +0200

    Add guardian and comeonin deps, configure

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Reverie.Mixfile do
   def application do
     [mod: {Reverie, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -36,6 +36,8 @@ defmodule Reverie.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
      {:cors_plug, "~> 1.1"},
+     {:comeonin, "~> 2.4"},
+     {:guardian, "~> 0.14.0"},
      {:cowboy, "~> 1.0"}]
   end
 
