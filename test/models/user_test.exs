5011f3e3e3565438b3e722afe18badfe22ac2b63
fpapado <fgpapado@gmail.com>
2017-01-03 02:48:13 +0200

    Add user model and migration

---
+++ test/models/user_test.exs
@@ -0,0 +1,40 @@
+defmodule Reverie.UserTest do
+  use Reverie.ModelCase
+
+  alias Reverie.User
+
+  @valid_attrs %{email: "fp@example.com",
+    password: "abcd1234",
+    password_confirmation: "abcd1234"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "mis-matched password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2bj1rjbk2",
+      password_confirmation: "b1bk23jkn12"})
+    refute changeset.valid?
+ end
+
+  test "missing password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2bj1rjbk2"})
+    refute changeset.valid?
+  end
+
+  test "short password doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1234a",
+      password_confirmation: "1234a"})
+    refute changeset.valid?
+  end
+end
