c0dc5ea63100586fa463a95d6c74947c1d169270
fpapado <fgpapado@gmail.com>
2017-03-24 13:23:16 +0000

    Limit pool size to allow for spare workers
    Heroku's free tier has a limit of 20 connections, so we should
    leave a couple spare for worker processes (seeds, debugging).

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :reverie, Reverie.Endpoint,
 config :reverie, Reverie.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20
+  pool_size: 18
 
 # Do not print debug messages in production
 config :logger, level: :info
